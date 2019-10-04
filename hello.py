a10b742f2d3ad16ab532ea6100043836ad1a54d7
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-10-04 14:26:31 +0200

    Write yaml to file

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from yaml import load, dump
 
 
-with open("deploy.yaml", "r") as stream:
-    data = load(stream)
+with open("input.yaml", "r") as in_stream:
+    data = load(in_stream)
     print(data)
+
+    with open("output.yaml", "w") as out_stream:
+        out_stream.write(dump(data))
