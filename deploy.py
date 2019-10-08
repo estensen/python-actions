9f5cf8afb272edbaa64d7f63260c30e4581c1da5
Håvard Anda Estensen <haavard.ae@gmail.com>
2019-10-08 13:18:15 +0200

    Allow deployment of multiple pipelines

--- deploy.py
+++ deploy.py
@@ -3,26 +3,20 @@ from yaml import load, dump, Loader
 
 with open("pipelines.yaml", "a+") as production_file:
     production_file.seek(0)  # Hack to both read and write
-    pipelines = load(production_file, Loader)
+    deployed_pipelines = load(production_file, Loader)
 
     with open("pipeline.yaml", "r+") as deploy_file:
-        pipeline = load(deploy_file, Loader)
-        if not pipeline:
+        pipeline_updates = load(deploy_file, Loader)
+        if not pipeline_updates:
             exit("Empty deployment file")
 
-        name = list(pipeline.keys())[0]
-        data = pipeline[name]
-        pipelines[name] = data
-
-        if not pipelines:
-            keys = set()
-            print("No pipelines in production")
-        else:
-            keys = set(pipelines.keys())
-            print(f"Pipelines in production: {keys}")
-
         print("Deploying...")
-        print(f"Deployed {data}")
+        for pipeline in pipeline_updates:
+            print(pipeline)  # Or actually run a deployment script
+            data = pipeline_updates[pipeline]
+            deployed_pipelines[pipeline] = data
+
         production_file.truncate(0)  # Remove content from file
-        production_file.write(dump(pipelines))
+        production_file.write(dump(deployed_pipelines))
         deploy_file.truncate(0)
+        print("Deployment complete")
